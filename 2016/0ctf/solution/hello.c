adff5f7932f28a552228af835d556a0544d2b949
wen.xu@gatech.edu <wxu92@gatech.edu>
2018-03-12 20:40:24 -0400

    x

---
+++ 2016/0ctf/solution/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	//printf("Hello World!\n");
+	while(1)
+	{
+		sleep(10);
+	}
+	return 0;
+}
---
+++ 2016/0ctf/solution/try.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ for i in `seq 64`; do ./hello & done
